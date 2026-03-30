cd7108b1c95cc6f77c2cb291872e90854faa0f24
MD-ELIUS <mdelius.nfs@gmail.com>
2026-03-30 12:42:22 +0600

    feat: add story page in dashboard

---
+++ src/app/dashboard/add-story/page.jsx
@@ -0,0 +1,10 @@
+import Title from '@/components/Title'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <Title>Add your story</Title>
+    </div>
+  )
+}
