bcbbc32c58f5a92f4af5f793fa5bcf38b5f18cf2
MD-ELIUS <mdelius.nfs@gmail.com>
2026-03-30 12:42:23 +0600

    refactor: update Title component implementation

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Title({children}) {
   return (
-    <div className='bg-linear-60 from-sky-50 to-sky-200 p-3 rounded text-black'>
+    <div className='bg-linear-60 from-sky-50 to-sky-200 p-3 rounded text-black text-center'>
         <h2 className='text-5xl font-bold'>{children}</h2>
     </div>
   )
