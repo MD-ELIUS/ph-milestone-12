1acd28d1ea935482f006bd717ff8137532d0727b
MD-ELIUS <mdelius.nfs@gmail.com>
2026-03-31 15:32:22 +0600

    refactor: update dashboard layout styling and metadata

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -5,7 +5,7 @@ export default function layout( {children} ) {
   return (
     <div className='grid grid-cols-12 gap-5 min-h-screen'>
        <div className='col-span-3 border-r-2'>
-         <h2>Navigation</h2>
+             <Link href="/" className='text-lg font-semibold'> Dev Story </Link> 
          <div className='flex flex-col gap-5 mt-10 mr-4'>
             <Link className='py-2 px-4 w-full bg-green-500' href="/dashboard/add-story">Add Story</Link>
             <Link className='py-2 px-4 w-full bg-green-500' href="/dashboard/my-profile">My Profile</Link>
