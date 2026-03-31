f26e7e90b60fa6761267fbadfd1fcaff21042c57
MD-ELIUS <mdelius.nfs@gmail.com>
2026-03-31 15:28:22 +0600

    feat: update header layout and styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,25 @@
+"use client"
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
+import NavLink from './NavLink';
 
 export default function Header() {
+  const pathname = usePathname() ;
+  if (pathname.includes('dashboard') )  { 
+    return <></>
+  }
   return (
     <div>
         <header className=" border-b-2 border-gray-500 px-4 py-3 flex justify-between items-center flex-wrap">
           <Link href="/" className='text-lg font-semibold'> Dev Story </Link> 
 
           <nav className='space-x-8'>
-            <Link href="/about">about</Link>
-            <Link href="/stories">stories</Link>
-            <Link href="/tutorials">tutorials</Link>
-            <Link href="/login">login</Link>
-            <Link href="/register">register</Link>
+            <NavLink href="/about">about</NavLink>
+            <NavLink href="/stories">stories</NavLink>
+            <NavLink href="/tutorials">tutorials</NavLink>
+            <NavLink href="/login">login</NavLink>
+            <NavLink href="/register">register</NavLink>
           </nav>
         </header>
 
