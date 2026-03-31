7ad15bbc473ae45d1f8c5db7c95a17b412531542
MD-ELIUS <mdelius.nfs@gmail.com>
2026-03-31 15:19:04 +0600

    feat: add loading skeleton for story detail page

---
+++ src/app/stories/[id]/loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function loading() {
+  return (
+<div>
+        <div className='min-h-screen flex justify-center items-center text-8xl'>
+            <h2>Loading</h2> <span className='animate-spin'>...</span>
+
+        </div>
+    </div>
+  )
+}
